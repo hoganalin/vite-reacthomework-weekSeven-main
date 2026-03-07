a9554b69634dd61d1995ba1de85d3b502d1bfe9c
hoganalin <hoganalin@gmail.com>
2026-03-07 18:02:48 +0800

    更新vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base:
     process.env.NODE_ENV === 'production'
-      ? '/vite-reacthomework-weekSeven/'
+      ? '/vite-reacthomework-weekSeven-main/'
       : '/',
   plugins: [react()],
 });
